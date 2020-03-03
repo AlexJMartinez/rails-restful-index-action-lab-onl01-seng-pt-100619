ae6f41b3cf7362ba073fcc5b610cab6323c15439
Alex Martinez <alexandr3martin3z@gmail.com>
2020-03-03 01:45:08 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-get '/students' => 'students#index'
+get '/students' => 'students#index', :as => :students
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
